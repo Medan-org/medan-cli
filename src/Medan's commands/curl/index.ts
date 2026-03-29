ccf20f068addefdf34724cca76e6e2ff0eb70576
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-29 20:19:22 +0100

    Update index.ts

--- src/Medan's commands/curl/index.ts
+++ src/Medan's commands/curl/index.ts
@@ -4,8 +4,8 @@ export default function curlCommand() {
   const cmd = new Command("curl");
 
   cmd
-    .description("Fetch a URL")
     .argument("<url>", "URL to fetch")
+    .description("Fetch a URL")
     .action((url) => {
       console.log(`Curl is not implemented yet. Would fetch: ${url}`);
     });
