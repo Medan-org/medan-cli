b2908c7d05fbae293d9dde9ad1a42d7beced67a1
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-29 20:09:03 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Command } from "commander";
+import { readdirSync } from "fs";
+import { join } from "path";
 
 const program = new Command();
 
@@ -7,11 +9,20 @@ program
   .description("The Medan command-line interface")
   .version("0.1.0");
 
-program
-  .command("hello")
-  .description("Say hello from Medan CLI")
-  .action(() => {
-    console.log("Hello from Medan CLI!");
-  });
+// Load built-in commands
+const commandsDir = join(__dirname, "Medan's commands");
+
+const commandFolders = readdirSync(commandsDir, { withFileTypes: true })
+  .filter(dir => dir.isDirectory())
+  .map(dir => dir.name);
+
+for (const folder of commandFolders) {
+  const commandPath = join(commandsDir, folder, "index.js");
+  const commandModule = require(commandPath);
+
+  if (typeof commandModule.default === "function") {
+    program.addCommand(commandModule.default());
+  }
+}
 
 program.parse();
